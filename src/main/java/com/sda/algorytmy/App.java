b481447c3e9c9817d25a818e3484eee3f52a23bf
Jacek.Woj <jacek.wojtuszek@gmail.com>
2019-05-12 14:30:00 +0100

    zliczanie_powtorzen

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,15 @@
 package com.sda.algorytmy;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        int N = 5;
+        int wynik = 0;
+        int[] tab = {2, 3, 4, 2, 5, 6, 2, 5, 2, 2, 2, 2, 4};
+        for (int i = 0; i<tab.length; i++){
+            if (tab[i] == N){
+                wynik += 1;
+            }else;
+        }
+        System.out.println("Wartość " + N + " wystąpiła w tablicy " + wynik + " razy.");
     }
 }
